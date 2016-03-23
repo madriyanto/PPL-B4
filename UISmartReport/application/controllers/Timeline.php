81ae8d3840b41e1eb3fa5a5d8834e67663817e99
Muhammad Adriyanto <madriyanto27@gmail.com>
2016-03-23 22:43:30 +0700

    Update Controller and View for Homepage

---
+++ UISmartReport/application/controllers/Timeline.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Timeline extends CI_Controller {
+
+	public function __construct()
+    {
+		parent::__construct();
+		$this->load->library('session');
+		$this->load->helper('url');
+		$this->load->model('Timeline_model');
+    }
+
+	public function index()
+	{
+		$session_id = $this->session->userdata('username');
+		if(!isset($session_id)))
+		{
+			redirect('login');
+		}
+		else
+		{
+			$this->load->helper(array('form', 'url'));
+
+			$this->load->library('form_validation');
+
+			$this->form_validation->set_rules('post', 'Post', 'required');
+
+			if ($this->form_validation->run() == FALSE)
+			{
+				$data['timeline'] = $this->Timeline_model->retrieve_posts();
+				$this->load->view('tlview', $data);
+			}
+			else
+			{
+				if($this->input->post('anonymous') == 'true')
+				{
+					$newdata1 = array(
+					        'OwnerId'  => $session_id,
+					        'Data' => $this->input->post('post'),
+					        'IsAnonymous' => true
+					);
+				}
+				else
+				{
+					$newdata1 = array(
+					        'OwnerId'  => $session_id,
+					        'Data' => $this->input->post('post')
+					);
+				}
+				$this->Timeline_model->insert_post($newdata1);
+				$data['timeline'] = $this->Timeline_model->retrieve_posts();
+				$this->load->view('tlview', $data);
+			}
+		}
+	}
+}
---
+++ UISmartReport/application/views/homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Example</title>
+</head>
+<body>
+
+<div id="container">
+	<h5>HOMEPAGE</h5>
+	<p>Terms and Conditions</p>
+	<a href='index.php/login'>I Agree</a>
+</div>
+
+</body>
+</html>
