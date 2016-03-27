6df65cf728a73d311c343a235dc32cef8bc55dca
Dennis Pratama Kamah <dkamah913@gmail.com>
2016-03-27 19:00:35 +0700

    Added files via upload

---
+++ UISmartReport/application/views/templates/footer.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container-fluid bg-4 text-center">
+    <h4><font color="white">Copyright &#169; 2016, PPL B-04</font></h4>
+  </div> 
+</nav>
+</body>
+</html>
