416a54d208bb59eb649e079156eb40e043ebe5f2
Dennis Pratama Kamah <dkamah913@gmail.com>
2016-03-28 16:13:22 +0700

    Update loginspacc.php

--- UISmartReport/application/views/loginspacc.php
+++ UISmartReport/application/views/loginspacc.php
@@ -35,7 +35,7 @@
     <div class="col-sm-4 loginForm">
 		<h2 class="text-center"><b>Login</b></h2>
 		<h4 class="text-center">Please fill your username and password</h4><br>
-		<form role="form" name="myForm" action="<?php echo base_url('index.php/loginsp'); ?>" method="post" accept-charset="utf-8">
+		<form role="form" name="myForm" action="<?php echo base_url('index.php/Loginsp'); ?>" method="post" accept-charset="utf-8">
 			<?php echo validation_errors(); ?>
 			<div class="form-group">
 				<label for="text">Username</label>
@@ -45,7 +45,7 @@
 				<label for="text">Password</label>
 				<input type="password" class="form-control" name="password" required/>
 			</div>
-			Forgot your password? Click <a href="<?php echo base_url('index.php/forgetpassword/index'); ?>">here</a><br><br>
+			Forgot your password? Click <a href="<?php echo base_url('index.php/ForgetPassword/index'); ?>">here</a><br><br>
 			<button class="btn btn-primary btn-lg" type="submit" value="Submit">Login</button>
 		</form>
     </div>
