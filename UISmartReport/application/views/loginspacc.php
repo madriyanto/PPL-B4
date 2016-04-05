eff818e9ab2656087c8b1d7d5df79a821e21d9bb
bimaaatyo <bima.arthaprasetyo@gmail.com>
2016-04-05 18:26:12 +0700

    Update loginspacc.php

--- UISmartReport/application/views/loginspacc.php
+++ UISmartReport/application/views/loginspacc.php
@@ -1,42 +1,91 @@
 <style>
+	body {
+    	background-color: #E4E4E4;
+  	}
+
 	.btn-primary {
 		background: #FFFF00;
 		color: black;
 		border-style: none;
 		font-size: 25px;
-		padding: 15px 50px;
-		margin-left: 110px;
-		margin-bottom: 40px;
+		padding : 15px 30px 14px 30px;
+    	margin  : 0px auto;
+    	display : block;
+    	width   : 150px;
 	}
+	
 	.loginForm {
+		background-color: white; 
 		border: 5px solid;
 		padding-left: 40px;
 		padding-right: 40px;
+		margin: 0 auto;
+		margin-top: 1%;
 	}
+
+	#font1{
+		text-align: center;
+    	font-size: 40px;
+	}
+	#font2{
+		text-align: justify;
+    	font-size: 16px;
+	}
+	.row{
+		margin-right: 0px;
+	}
+	
+	@media screen and (max-width:768px){
+		.loginForm{
+			width: 80%;
+			margin-bottom: 20%;
+		}
+	}
+
+	@media screen and (max-height:500px){
+		.row{
+			height: 90%;
+		}
+
+		.loginForm{
+			margin-bottom: 10%;
+		}
+
+	}
+
 </style>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<div class="navbar-brand">
-			<img src="<?php echo base_url('assets/images/makara.png'); ?>" class="img-rounded" alt="Cinque Terre" width="30" height="30"></div>
-			<div class="navbar-brand">UI Smart Report</div>
+       		 <div class="navbar-brand">UI Smart Report
+       		 </div>
+
+       		 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>                        
+      		 </button>
+		</div>		
+		
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<a href="<?php echo base_url('index.php/Login/index'); ?>"><li class="navbar-brand">Login SSO</li></a>
+				<li class="navbar-brand">About Us</li>
+			</ul>
 		</div>
-		<ul class="nav navbar-nav navbar-right">
-			<a href="<?php echo base_url('index.php/Login/index'); ?>"><li class="navbar-brand">Login SSO</li></a>
-			<li class="navbar-brand">About Us</li>
-		</ul>
 	</div>
 </nav>
 <br><br><br><br>
 <div class="row">
 	<div class="col-sm-4"></div>
     <div class="col-sm-4 loginForm">
-		<h2 class="text-center"><b>Login</b></h2>
-		<h4 class="text-center">Please fill your username and password</h4><br>
+    	<br>
+		<p id="font1"><b>Login</b></p>
+		<p id="font2">Please fill your username and password</p><br>
 		<form role="form" name="myForm" action="<?php echo base_url('index.php/Loginsp'); ?>" method="post" accept-charset="utf-8">
-			<?php echo $result; ?>
+			<?php echo validation_errors(); ?>
 			<div class="form-group">
 				<label for="text">Username</label>
 				<input type="text" class="form-control" name="username" value="<?php echo set_value('username'); ?>" required/>
