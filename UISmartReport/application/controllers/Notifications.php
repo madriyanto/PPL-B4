73c0ecc4973a903ad192f9ffe455c0884804ca10
Muhammad Adriyanto <madriyanto27@gmail.com>
2016-04-29 13:11:13 +0700

    Revert "Update Notifications"
    This reverts commit f254304bcb102c35cdbdf97b291393512d48402e.

--- UISmartReport/application/controllers/Notifications.php
+++ UISmartReport/application/controllers/Notifications.php
@@ -8,7 +8,6 @@ class Notifications extends CI_Controller {
 		parent::__construct();
 		$this->load->library('session');
 		$this->load->helper('url');
-		$this->load->helper('date');
 		$this->load->model('Loginuser_model');
 		$this->load->model('Loginsp_model');
 		$this->load->model('Notification_model');
@@ -19,14 +18,10 @@ class Notifications extends CI_Controller {
 		$session_id = $this->session->userdata('username');
 		if(isset($session_id)) {
 			$data['notif'] = $this->Notification_model->get($session_id);
-			$data['count_notif'] = $this->Notification_model->count_notif($this->session->userdata('username'));
-			$datahead['title'] = 'Notifications';
-			$this->load->view('templates/header', $datahead);
 			$this->load->view('notif', $data);
-			$this->load->view('templates/footer');
 		}
 		else {
-			redirect(base_url());
+			redirect('Timeline');
 		}
 	}
 }
--- UISmartReport/application/views/postview.php
+++ UISmartReport/application/views/postview.php
@@ -1,10 +1,269 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Example</title>
+<!--	
+	<style>
+		body {
+			background-color: #E4E4E4;
+			font-family: Verdana, Geneva, sans-serif;
+			font-size: 18px;
+		}
+	
+		.navbar-bottom {
+			margin-bottom: 0px;
+			bottom: 0;
+			position: fixed;
+			width: 100%;
+		}
+		
+		.waktu {
+			margin-right: 30px;
+			text-align: right;
+		}
+		
+		.tombol {
+
+			text-align: right;
+		}
+		
+		.profpic {
+			margin-left: 20px; 
+		}
+		
+		.post {
+			margin-left: 30px;
+		}
+	</style>
+	<script>
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip();  
+			$("#editPost").click(function(){
+				$("#postModal").modal();
+			});
+			$("#deletePost").click(function(){
+				$("#deleteModal").modal();
+			});
+			$("#pinPost").click(function(){
+				$("#pinModal").modal();
+			});
+			$("#closePost").click(function(){
+				$("#closeModal").modal();
+			});
+		});
+		
+	</script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+       		 <div class="navbar-brand">UI Smart Report</div>
+       		 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>                        
+      		 </button>
+		</div>		
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<a href="#"><li class="navbar-brand">Login SSO</li></a>
+				<li class="navbar-brand">About Us</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<br><br><br>
+<div class="container">
+	<div class="well">
+		<div class="row">
+			<div class="col-sm-2 profpic">
+				<img src="assets/images/makara.png" class="img-rounded" alt="Cinque Terre" width="150" height="150"> 
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-12">Haryanto Rio</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">to BemPacil, FUKIPacil</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<button type="button" class="btn btn-default btn-sm"></button>&nbsp Mentorship
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-1 waktu">3 s</div>
+		</div>
+		<br>
+		<div class="row post">
+			keren banget acaranya ini. Nggak ada kekurangannya sama sekali. Fix tahun depan dateng lagi :)
+		</div>
+		<div class="row tombol">
+			<div class="col-sm-2 profpic"></div>
+			<div class="col-sm-7"></div>
+			<div class="col-sm-2 tombol">
+				<button type="button" id="editPost" class="btn btn-default btn-lg" data-toggle="tooltip" title="Edit This Post"></button>
+				<div id="postModal" class="modal fade" role="dialog">
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+							<div class="modal-header" style="text-align: center;">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Edit This Post</h4>
+							</div>
+							<div class="modal-body" style="text-align: left;">
+								<form role="form">
+									<div class="form-group">
+										<label for="textarea">Description:</label>
+										<textarea class="form-control" id="post" name="post" rows="6" placeholder="Description of Post" required></textarea>
+									</div>
+									<div class="form-group">
+										<label for="text">To:</label>
+										<input type="text" class="form-control" id="mention" name="mention" placeholder="To Organization" required>
+									</div>
+									<div class="form-group">
+										<label for="text">Event:</label>
+										<input type="text" class="form-control" id="title" name="title" placeholder="An Event" required>
+									</div>
+									<div class="form-group">
+										<div class="checkbox">
+											<label><input type="checkbox" id="anonymous" name="anonymous" value="true">Anonymous</label>
+										</div>
+									</div>
+									<div class="form-group">
+										<input type="file" id="userfile" name="userfile">
+									</div>
+									<button type="submit" class="btn btn-primary btn-lg" value="Submit">Edit Post</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<button type="button" id="deletePost" class="btn btn-default btn-lg" data-toggle="tooltip" title="Delete This Post"></button>
+				<div id="deleteModal" class="modal fade" role="dialog">
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+							<div class="modal-header" style="text-align: center;">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Confirmation</h4>
+							</div>
+							<div class="modal-body" style="text-align: left;">
+								<form role="form">
+									<div class="form-group">
+										<label for="textarea">Are you sure you want to delete this post ?</label>
+									</div>
+									<div class="form-group">
+										<button type="button" class="btn btn-primary btn-lg" value="Yes">Yes</button>
+										<button type="button" class="btn btn-danger btn-lg active" value="No" data-dismiss="modal">No</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<button type="button" id="pinPost" class="btn btn-default btn-lg" data-toggle="tooltip" title="Pin This Post"></button>
+				<div id="pinModal" class="modal fade" role="dialog">
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+							<div class="modal-header" style="text-align: center;">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Confirmation</h4>
+							</div>
+							<div class="modal-body" style="text-align: left;">
+								<form role="form">
+									<div class="form-group">
+										<label for="textarea">Are you sure you want to pin this post ?</label>
+									</div>
+									<div class="form-group">
+										<button type="button" class="btn btn-primary btn-lg" value="Yes">Yes</button>
+										<button type="button" class="btn btn-danger btn-lg active" value="No" data-dismiss="modal">No</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<button type="button" id="closePost" class="btn btn-default btn-lg" data-toggle="tooltip" title="Close This Post (Thread)"></button>
+				<div id="closeModal" class="modal fade" role="dialog">
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+							<div class="modal-header" style="text-align: center;">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Confirmation</h4>
+							</div>
+							<div class="modal-body" style="text-align: left;">
+								<form role="form">
+									<div class="form-group">
+										<label for="textarea">Are you sure you want to close this post (thread) ?</label>
+									</div>
+									<div class="form-group">
+										<button type="button" class="btn btn-primary btn-lg" value="Yes">Yes</button>
+										<button type="button" class="btn btn-danger btn-lg active" value="No" data-dismiss="modal">No</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="well">
+		<form role="form">
+			<div class="form-group">
+				<label for="comment">Your Comment</label>
+				<textarea class="form-control" rows="4" id="comment"></textarea>
+				<br>
+				<button type="submit" class="btn btn-warning">Post Comment</button>
+			</div>
+		</form>
+		<hr width="100%">
+		<p><b>Last Comment</b></p>
+		<div class="row">
+			<div class="col-sm-2 profpic">
+				<img src="assets/images/makara.png" class="img-rounded" alt="Cinque Terre" width="150" height="150"> 
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-12 lead">Dzulfikar</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">Wew, Haryanto komentarnya. Gils gils.</div>
+				</div>
+			</div>
+			<div class="col-sm-1 waktu">1 s</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-sm-2 profpic">
+				<img src="assets/images/makara.png" class="img-rounded" alt="Cinque Terre" width="150" height="150"> 
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-12 lead">Dzulfikar</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">Wew, Haryanto komentarnya. Gils gils.</div>
+				</div>
+			</div>
+			<div class="col-sm-1 waktu">1 s</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-sm-2 profpic">
+				<img src="assets/images/makara.png" class="img-rounded" alt="Cinque Terre" width="150" height="150"> 
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-12 lead">Dzulfikar</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">Wew, Haryanto komentarnya. Gils gils.</div>
+				</div>
+			</div>
+			<div class="col-sm-1 waktu">1 s</div>
+		</div>
+	</div>
+</div>
+<br><br>
+-->
 </head>
 <body>
 
