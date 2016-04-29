980df2457697dc596d80fb299fb2c2b78485835a
Dennis Pratama Kamah <dkamah913@gmail.com>
2016-04-29 17:28:00 +0700

    Fix Desain and PHP from me

--- UISmartReport/application/views/editview.php
+++ UISmartReport/application/views/editview.php
@@ -1,4 +1,3 @@
-<!--
 <style>
 		body {
 			background-color: #E4E4E4;
@@ -16,13 +15,50 @@
 			position: fixed;
 			width: 100%;
 		}
+		
+		.tombol {
+			text-align: center;
+		}
+		
+		.logout {
+			border-style: solid;
+			border-color: yellow;
+			margin-right: 30px;
+			padding-top: 10px;
+			padding-bottom: 10px;
+		}
 	</style>
+	<script>
+		$(document).ready(function(){
+			$('#mention').tokenfield({
+				autocomplete: {
+					<?php
+						echo "source: [";
+						foreach ($mention as $row){
+							echo '{label: \''.$row->Name.'\', value: \''.$row->Username.'\'}, ';
+						}
+						echo "],";
+					?>
+					delay: 100
+				},
+				showAutocompleteOnFocus: true
+			});
+	
+			$('#mention').on('tokenfield:createtoken', function (event) {
+				var existingTokens = $(this).tokenfield('getTokens');
+				$.each(existingTokens, function(index, token) {
+					if (token.value === event.attrs.value)
+						event.preventDefault();
+					});
+				});
+			});	
+	</script>
 </head>
 <body>
 <nav class="navbar-inverse navbar-fixed-top">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<div class="navbar-brand"><img src="assets/images/makara.png" class="img-rounded" alt="Cinque Terre" width="30" height="30"></div>
+			<div class="navbar-brand"><img src="<?php echo base_url('assets/images/makara.png'); ?>" class="img-rounded" alt="Cinque Terre" width="30" height="30"></div>
 			<div class="navbar-brand">UI Smart Report</div>
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 				<span class="icon-bar"></span>
@@ -35,77 +71,72 @@
         		<li><a href="#">About Us</a></li>
         	</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li><span class="navbar-brand"><a href="#" data-toggle="modal" data-target="#myModal">Logout</a></span></li>
+				<li><span class="navbar-brand logout"><a href="#" data-toggle="modal" data-target="#myModal">Logout</a></span></li>
 			</ul>
 		</div>
 	</div>
 </nav>
 <br><br><br>
+<!-- Logout Modal-->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Are you sure you want to log out?</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+        <a href="<?php echo base_url('logout'); ?>" class="btn btn-primary" role="button">Yes</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End of Logout Modal-->
 <div class="container">
 	<div class="well">
 		<h2 style="text-align: center;">Edit Post</h2>
+		<?php echo validation_errors(); ?>
+		<?php echo $error; ?>
+
+		<?php echo form_open_multipart('Post/edit/'.$Id); ?>
 		<form role="form">
 			<div class="form-group">
 				<label for="text">Event:</label>
-				<input type="text" class="form-control" id="title" name="title" placeholder="Enter event" required>
+				<input type="text" class="form-control" id="title" name="title" placeholder="Enter event" value="<?php echo $Title; ?>" required>
 			</div>
 			<div class="form-group">
 				<label for="textarea">Description:</label>
-				<textarea class="form-control" id="post" name="post" rows="6" placeholder="Enter your post description" required></textarea>
+				<textarea class="form-control" id="post" name="post" rows="6" placeholder="Enter your post description" required><?php echo $Data; ?></textarea>
 			</div>
 			<div class="form-group">
 				<label for="text">To:</label>
-				<input type="text" class="form-control" id="mention" name="mention" placeholder="To Organization" required>
+				<?php
+					foreach ($mention as $row) {
+						if($this->Post_model->is_mentioned($Id, $row->Username))
+						{
+							echo "<input type=\"checkbox\" name=\"mention[]\" value=\"".$row->Username."\" checked>".$row->Name;
+						}
+						else
+						{
+							echo "<input type=\"checkbox\" name=\"mention[]\" value=\"".$row->Username."\">".$row->Name;
+						}
+					}
+				?>
 			</div>
 			<div class="form group">
-				<input type="checkbox" id="anonymous" name="anonymous" value="true"> Anonymous
+				<input type="checkbox" id="anonymous" name="anonymous" value="true" <?php if($IsAnonymous) echo "checked"; ?>> Anonymous
 			</div>
 			<br>
 			<div class="form group">
-				<input type="file" id="userfile" name="userfile">
+				<input type="file" id="userfile" name="userfile" value="<?php echo $Attachments; ?>">
 			</div><br>
-			<button type="submit" class="btn btn-primary btn-lg" value="Submit">Edit Post</button>
-			<a href="postview.html"><button type="submit" class="btn btn-danger btn-lg" value="Batal">Kembali ke Halaman Sebelumnya</button></a>
+			<div class="form group tombol">
+				<button type="submit" class="btn btn-primary btn-lg" value="Submit">Edit Post</button>
+				<a href="postview.html"><button type="submit" class="btn btn-danger btn-lg" value="Batal">Kembali ke Halaman Sebelumnya</button></a>
+			</div>
 		</form>
 	</div>
 </div>
 <br><br>
--->
-</head>
-<body>
 
-<div id="container">
-	<a href=<?php echo "\"".base_url()."index.php\""; ?>><h3>Homepage</h3></a>
-	<a href=<?php echo "\"".base_url()."index.php/Profile\""; ?>><h3>Profile</h3></a>
-	<a href=<?php echo "\"".base_url()."index.php/Close\""; ?>><h3>Log Out</h3></a>
-
-	<?php echo validation_errors(); ?>
-	<?php echo $error; ?>
-
-	<?php echo form_open_multipart('Post/edit/'.$Id); ?>
-
-	<label>Title</label>
-	<input type="text" name="title" value=<?php echo "\"$Title\""; ?> size="50" />
-	<br/><label>Post</label><br/>
-	<textarea name="post" rows="5" cols="50" style="resize:none"><?php echo $Data; ?></textarea>
-	<br/><label>To: </label>
-	<?php
-		foreach ($mention as $row) {
-			if($this->Post_model->is_mentioned($Id, $row->Username))
-			{
-				echo "<input type=\"checkbox\" name=\"mention[]\" value=\"".$row->Username."\" checked>".$row->Name;
-			}
-			else
-			{
-				echo "<input type=\"checkbox\" name=\"mention[]\" value=\"".$row->Username."\">".$row->Name;
-			}
-		}
-	?>
-	<br/><label>Attachment</label>
-	<input type="file" name="userfile" value="<?php echo "\"$Attachments\""; ?>" size="20" />
-	<label>Post as Anonymous</label>
-	<input type="checkbox" name="anonymous" value="true" <?php if($IsAnonymous) echo "checked"; ?>/>
-	<div><input type="submit" value="Submit" /></div>
-
-	</form>
-</div>
