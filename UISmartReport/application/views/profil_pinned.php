fa27782137b1e8530987d900d030318865a4a4c4
bimaaatyo <bima.arthaprasetyo@gmail.com>
2016-04-30 20:18:00 +0700

    Update profil_pinned.php

--- UISmartReport/application/views/profil_pinned.php
+++ UISmartReport/application/views/profil_pinned.php
@@ -4,6 +4,12 @@
     background-color: #E4E4E4;
   }
 
+  .box-post {
+		background-color: white;
+    	border-radius: 20px;
+    	margin-bottom: 10px;
+	}
+	
   #navbarbawah a{
     color: black;
   }
@@ -183,7 +189,7 @@
       <h3 class="text-left"><?php echo $Contact; ?></h3>
     </div>
 
-    <div class="col-md-3" id="coba">
+    <div class="col-md-3" id="kotakBO">
       <br>
       <h4 class="text-left">Total Posts : <?php echo $count_posts; ?></h4>
       <h4 class="text-left">Closed Posts : <?php echo $count_closed_posts; ?></h4>
